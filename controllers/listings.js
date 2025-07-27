7c5574f81e95c48388d4884d1916a9ef20aac781
Tia Piol <tiarox02@yahoo.com>
2025-07-27 12:43:33 -0700

    build the listings landing page

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing");
 
-
+// GET /listings
+router.get("/", async (req, res) => {
+    try {
+        const allListings = await Listing.find({});
+        console.log(allListings)
+        res.send("listings index page")
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
+});
 
 
 module.exports = router;
